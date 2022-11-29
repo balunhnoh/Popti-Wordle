0e0798d1ba480f3bf58717c172b0436ded137f80
Edy López <balunhnoh@gmail.com>
2022-11-28 23:44:34 -0600

    Modificación de configuraciones
    Nuevos cambios

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
   language: 'Popti', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'edylopez', // Put your name here so people know who made this Wordle!
+  author: 'Edy B. Lopez', // Put your name here so people know who made this Wordle!
   authorWebsite: 'https://hinhat.es.tl/', // Put a link to your website or social media here
   wordListSource: 'YouTube: Hinhat', // Describe the source material for your words here
   wordListSourceLink: 'https://www.youtube.com/@hinhatpopti', // Put a link to the source material for your words here
